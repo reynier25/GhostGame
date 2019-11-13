7f12ba56e892263e199fb84d38a93873cd7034d7
reynier25 <fsoto@uchicago.edu>
2019-11-13 15:17:07 -0800

    game setup helper methods

---
+++ ghost.rb
@@ -0,0 +1,95 @@
+require "set"
+require_relative "player"
+
+class GhostGame
+  ALPHABET = Set.new("a".."z")
+  MAX_LOSS_COUNT = 5
+
+  def initialize(*players)
+    words = File.readlines("dictionary.txt").map(&:chomp)
+    @dictionary = Set.new(words)
+    @players = players
+    @losses = Hash.new { |losses, player| losses[player] = 0 }
+  end
+
+  def run
+    play_round until game_over?
+    puts "#{winner} wins!"
+  end
+
+  private
+  
+  attr_reader :fragment, :dictionary, :losses, :players
+
+  def game_over?
+    remaining_players == 1
+  end
+
+  def play_round
+    @fragment = ""
+    welcome
+
+    until round_over?
+      take_turn
+      next_player!
+    end
+
+    update_standings
+  end
+
+  # helper methods
+  def add_letter(letter)
+    fragment << letter
+  end
+
+  def valid_play?(letter)
+    return false unless ALPHABET.include?(letter)
+
+    potential_fragment = fragment + letter
+    dictionary.any? { |word| word.start_with?(potential_fragment) }
+  end
+
+  def is_word?(fragment)
+    dictionary.include?(fragment)
+  end
+
+  def round_over?
+    is_word?(fragment)
+  end
+
+  def current_player
+    players.first
+  end
+
+  def previous_player
+    (players.count - 1).downto(0).each do |idx|
+      player = players[idx]
+
+      return player if losses[player] < MAX_LOSS_COUNT
+    end
+  end
+
+  def next_player!
+    players.rotate!
+    # keep rotating until we find a player who hasn't been eliminated
+    players.rotate! until losses[current_player] < MAX_LOSS_COUNT
+  end
+
+  def remaining_players
+    losses.count { |_, v| v < MAX_LOSS_COUNT }
+  end
+
+  def winner
+    (player, _) = losses.find { |_, losses| losses < MAX_LOSS_COUNT }
+    player
+  end
+
+  def record(player)
+    count = losses[player]
+    "GHOST".slice(0, count)
+  end
+
+  # UI methods ?
+
+end
+
